7f7f45d519ceb7af35163544f2cd44d1fd0937cd
José Bruno <60969043+josebruno2020@users.noreply.github.com>
2021-04-10 23:40:42 -0300

    Controlando o acesso para modulos que não estão ativos

---
+++ public/js/module.js
@@ -0,0 +1,3 @@
+$(document).ready(function() {
+    $('.cursor-desabled').removeAttr('href');
+});
--- resources/views/layouts/layout-module.blade.php
+++ resources/views/layouts/layout-module.blade.php
@@ -14,20 +14,24 @@
 </head>
 <body>
     <header class="header mb-3">
-        <ul class="nav d-flex flex-row justify-content-between">
+        <ul class="container nav d-flex flex-row justify-content-between">
             <span class="d-flex">
                 @foreach ($modules as $module)
                     <li class="nav-item">
-                        <a class="nav-link {{$loggedUser->module_active == $module->id ? 'active' : ''}}" aria-current="page" href="{{route('module.index', [$module->slug])}}">{{$module->name}}
+                        <a 
+                            class="nav-link {{$loggedUser->module_active == $module->id ? 'active' : ''}} {{$loggedUser->module_active == $module->id ? 'cursor-desabled' : ''}}" 
+                            aria-current="page" 
+                            href="{{route('module.index', [$module->slug])}}"
+                            title="{{ $module->name }}"
+                            >
+                                {{$module->name}}
+                            @if($loggedUser->module_active > $module->id)
                             <i class="fas fa-check-circle" style="color:green;"></i>
+                            @else
+                            <i class="far fa-times-circle" style="color: red"></i>
+                            @endif
                         </a>
                     </li>
-                    {{-- <li class="nav-item">
-                        <a class="nav-link" href="#">Tema 2 <i class="far fa-times-circle" style="color: red"></i></a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">Tema 3 <i class="far fa-times-circle" style="color: red"></i></a>
-                    </li> --}}
                   @endforeach
             </span>
             <span>
@@ -50,5 +54,7 @@
 
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.1/dist/umd/popper.min.js" integrity="sha384-SR1sx49pcuLnqZUnnPwx6FCym0wLsk5JZuNx2bPPENzswTNFaQU1RDvt3wT4gWFG" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/js/bootstrap.min.js" integrity="sha384-j0CNLUeiqtyaRmlzUHCPZ+Gy5fQu0dQ6eZ/xAww941Ai1SxSY+0EQqNXNE6DZiVc" crossorigin="anonymous"></script>
+    <script src="{{ asset('js/jquery-3.5.1.min.js') }}"></script>
+    <script src="{{ asset('js/module.js') }}"></script>
 </body>
 </html>
