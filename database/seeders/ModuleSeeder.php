8f8707d5f9cfd58acfc33dc3972dafe42120f706
José Bruno <josebrunocampanholi@gmail.com>
2021-05-14 10:36:16 -0300

    Colocando questões no seed

--- database/seeders/ModuleSeeder.php
+++ database/seeders/ModuleSeeder.php
@@ -36,22 +36,8 @@ class ModuleSeeder extends Seeder
             '<iframe class="module_video" src="https://www.youtube.com/embed/HCGj_-sUZXw" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>'
         );
 
-        $module2Question1 = $this->createQuestionModule(
-            $module2->id, 
-            1, 
-            2, 
-            'Primeira questão de teste'
-        );
-        $this->createAnswerQuestion(
-            $module2Question1->id,
-            1,
-            'Opção 1'
-        );
-        $this->createAnswerQuestion(
-            $module2Question1->id,
-            2,
-            'Opção 2'
-        );
+        $this->createQuestionsModule2($module2);
+
         
 
 
@@ -136,39 +122,257 @@ class ModuleSeeder extends Seeder
             $preparatory->id,
             1,
             1,
-            'Pergunta 1: Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptatibus magnam reprehenderit voluptas nam delectus dolores accusamus itaque dolor distinctio, cumque vitae laborum aliquid, esse error ab exercitationem voluptate laudantium qui.'
+            'A ventilação mecânica propicia melhora das trocas gasosas e diminuição do trabalho respiratório, podendo ser utilizada de forma invasiva através de um tubo endotraqueal ou cânula de traqueostomia.'
         );
 
         $this->createAnswerQuestion(
             $preparatory1->id,
             1,
-            'Opção 1'
+            'Certo'
         );
 
         $this->createAnswerQuestion(
             $preparatory1->id,
             2,
-            'Opção 2'
+            'Errado'
+        );
+
+        $this->createAnswerQuestion(
+            $preparatory1->id,
+            3,
+            'Parcialmente'
         );
 
 
         $preparatory2 = $this->createQuestionModule(
             $preparatory->id,
             2,
-            2,
-            'Pergunta 2: Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptatibus magnam reprehenderit voluptas nam delectus dolores accusamus itaque dolor distinctio, cumque vitae laborum aliquid, esse error ab exercitationem voluptate laudantium qui.',
+            3,
+            'Parâmetros de ajustes iniciais de ventilação mecânica: modalidade PCV-VCV volume corrente de 6-10ml/kg/peso, Frequência respiratória entre 16-18rpm.',
         );
 
         $this->createAnswerQuestion(
             $preparatory2->id,
             1,
-            'Opção 1'
+            'Certo'
         );
 
         $this->createAnswerQuestion(
             $preparatory2->id,
             2,
-            'Opção 2'
+            'Errado'
         );
+
+        $this->createAnswerQuestion(
+            $preparatory2->id,
+            3,
+            'Parcialmente'
+        );
+
+
+        $preparatory3 = $this->createQuestionModule(
+            $preparatory->id,
+            3,
+            1,
+            'O volume corrente, quantidade de gás que está sendo expirada e, por conseguinte, insuflada ao paciente e que a administração excessiva deste pode resultar em volutrauma, inicialmente deve ser de 6ml/kg/peso?',
+        );
+
+        $this->createAnswerQuestion(
+            $preparatory3->id,
+            1,
+            'Certo'
+        );
+
+        $this->createAnswerQuestion(
+            $preparatory3->id,
+            2,
+            'Errado'
+        );
+
+        $this->createAnswerQuestion(
+            $preparatory3->id,
+            3,
+            'Parcialmente'
+        );
+
+        $preparatory4 = $this->createQuestionModule(
+            $preparatory->id,
+            4,
+            2,
+            'PEEP Pressão Positiva Expiratória devem ser utilizados de 5-10cmH2o e ajustados de acordo a avaliação beira leito do paciente (olhar clinico)?',
+        );
+
+        $this->createAnswerQuestion(
+            $preparatory4->id,
+            1,
+            'Certo'
+        );
+
+        $this->createAnswerQuestion(
+            $preparatory4->id,
+            2,
+            'Errado'
+        );
+
+        $this->createAnswerQuestion(
+            $preparatory4->id,
+            3,
+            'Parcialmente'
+        );
+
+
+        $preparatory5 = $this->createQuestionModule(
+            $preparatory->id,
+            5,
+            1,
+            'Na Ventilação não invasiva, utiliza-se uma interfase externa geralmente uma máscara facial?',
+        );
+
+        $this->createAnswerQuestion(
+            $preparatory5->id,
+            1,
+            'Certo'
+        );
+
+        $this->createAnswerQuestion(
+            $preparatory5->id,
+            2,
+            'Errado'
+        );
+
+        $this->createAnswerQuestion(
+            $preparatory5->id,
+            3,
+            'Parcialmente'
+        );
+    }
+
+    protected function createQuestionsModule2($module2)
+    {
+        $question1 = $this->createQuestionModule(
+            $module2->id, 
+            1, 
+            1, 
+            'Um dos critérios para indicação de ventilação mecânica: parada cardiorrespiratória, SARA, rebaixamento de nível de consciência.'
+        );
+
+        $this->createAnswerQuestion(
+            $question1->id,
+            1,
+            'Certo'
+        );
+        $this->createAnswerQuestion(
+            $question1->id,
+            2,
+            'Errado'
+        );
+
+        $this->createAnswerQuestion(
+            $question1->id,
+            3,
+            'Parcialmente'
+        );
+
+        $question2 = $this->createQuestionModule(
+            $module2->id, 
+            2, 
+            2, 
+            'PSV modo de disparo exclusivo do paciente, ventilação assisto-controlada: permite o controle mais adequado das pressões em vvaas e alveolares e ventilação controlado volume  VCV: ventilação depende da mecânica ventilatória do paciente?'
+        );
+
+        $this->createAnswerQuestion(
+            $question2->id,
+            1,
+            'Certo'
+        );
+
+        $this->createAnswerQuestion(
+            $question2->id,
+            2,
+            'Errado'
+        );
+
+        $this->createAnswerQuestion(
+            $question2->id,
+            3,
+            'Parcialmente'
+        );
+
+        $question3 = $this->createQuestionModule(
+            $module2->id, 
+            3, 
+            1, 
+            'Sinais clínicos/mecânicos da assincronia do paciente e ventilador mecânico sendo percebe-se esforço inspiratório do paciente, sudorese excessiva, observando o acompanhamento do ciclo fornecido pelo ventilador.'
+        );
+
+        $this->createAnswerQuestion(
+            $question3->id,
+            1,
+            'Certo'
+        );
+
+        $this->createAnswerQuestion(
+            $question3->id,
+            2,
+            'Errado'
+        );
+
+        $this->createAnswerQuestion(
+            $question3->id,
+            3,
+            'Parcialmente'
+        );
+
+
+        $question4 = $this->createQuestionModule(
+            $module2->id, 
+            4, 
+            2, 
+            'O balonete da prótese traqueal deve se manter entre 30-35cmH2o com o cuffometro?'
+        );
+
+        $this->createAnswerQuestion(
+            $question4->id,
+            1,
+            'Certo'
+        );
+
+        $this->createAnswerQuestion(
+            $question4->id,
+            2,
+            'Errado'
+        );
+
+        $this->createAnswerQuestion(
+            $question4->id,
+            3,
+            'Parcialmente'
+        );
+
+        $question5 = $this->createQuestionModule(
+            $module2->id, 
+            5, 
+            1, 
+            'Pneumonia associada a ventilação mecânica PAV: deve se manter uma PaCo2 entre 35-45 mmHg. Para isso deve se alterar a Frequência respiratória no ventilador?'
+        );
+
+        $this->createAnswerQuestion(
+            $question5->id,
+            1,
+            'Certo'
+        );
+
+        $this->createAnswerQuestion(
+            $question5->id,
+            2,
+            'Errado'
+        );
+
+        $this->createAnswerQuestion(
+            $question5->id,
+            3,
+            'Parcialmente'
+        );
+        
     }
 }
