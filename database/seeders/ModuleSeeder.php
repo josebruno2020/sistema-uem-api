eb72dd5658a85b7c373bfe8d24f79ad1bd51928b
José Bruno <60969043+josebruno2020@users.noreply.github.com>
2021-06-03 10:44:50 -0300

    Questões do modulo 3

--- database/seeders/ModuleSeeder.php
+++ database/seeders/ModuleSeeder.php
@@ -59,22 +59,7 @@ class ModuleSeeder extends Seeder
             '<iframe class="module_video" src="https://www.youtube.com/embed/HCGj_-sUZXw" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>'
         );
 
-        $this->createQuestionModule(
-            $module3->id, 
-            1, 
-            2, 
-            'Primeira questão de teste', 
-            1, 
-            'Resposta 1'
-        );
-        $this->createQuestionModule(
-            $module3->id, 
-            2, 
-            1, 
-            'Segunda questão de teste', 
-            1, 
-            'Resposta 2'
-        );
+        $this->createQuestionsModule3($module3);
     }
 
     protected function createQuestionModule(int $moduleId, int $number, int $correct, string $question) : Question
@@ -373,6 +358,320 @@ class ModuleSeeder extends Seeder
             3,
             'Parcialmente'
         );
+
+       
         
+    }
+
+    protected function createQuestionsModule3($module3) 
+    {
+        $question1 = $this->createQuestionModule(
+            $module3->id, 
+            1, 
+            2, 
+            'Em relação a pré-oxigenação, marque a afirmação incorreta.'
+        );
+
+        $this->createAnswerQuestion(
+            $question1->id,
+            1,
+            'A pre-oxigenação pode ser realizada mediante uso de VNI (ventilação não invasiva), antes da intubação orotraqueal'
+        );
+        $this->createAnswerQuestion(
+            $question1->id,
+            2,
+            'O objetivo de realizar a pré-oxigenação, é definir se realmente o paciente necessita da intubação orotraqueal, só depois da pré-oxigenação definirá se será feito ou não o procedimento'
+        );
+
+        $this->createAnswerQuestion(
+            $question1->id,
+            3,
+            'O uso de máscara não reinalante associado ao cateter nasal pode ser utilizado como pré-oxigenação'
+        );
+
+        $this->createAnswerQuestion(
+            $question1->id,
+            4,
+            'A pré-oxigenação tem objetivo de aumentar o aporte de oxigênio, para que o paciente, antes do procedimento de intubação orotraqueal, possa suportar o período de apneia durante o procedimento de via aérea definitiva'
+        );
+
+
+        $question2 = $this->createQuestionModule(
+            $module3->id, 
+            2, 
+            3, 
+            'Na laringoscopia, qual estrutura deve ter a melhor visualização durante a passagem do tubo orotraqueal, para ter certeza de da intubação orotraqueal correta.'
+        );
+
+
+        $this->createAnswerQuestion(
+            $question2->id,
+            1,
+            'Valécula'
+        );
+        $this->createAnswerQuestion(
+            $question2->id,
+            2,
+            'Epiglote'
+        );
+
+        $this->createAnswerQuestion(
+            $question2->id,
+            3,
+            'Cordas Vocais'
+        );
+
+        $this->createAnswerQuestion(
+            $question2->id,
+            4,
+            'Base da língua'
+        );
+
+        $question3 = $this->createQuestionModule(
+            $module3->id, 
+            3, 
+            4, 
+            'Nem todas as intubações há visualização da introdução do tubo na traqueia. Existem indicativos que, se somados, dão segurança para a confirmação da intubação orotraqueal, exceto:'
+        );
+
+        $this->createAnswerQuestion(
+            $question3->id,
+            1,
+            'Capnografia com presença de CO2 formando curva em forma de onda'
+        );
+        $this->createAnswerQuestion(
+            $question3->id,
+            2,
+            'Ausculta pulmonar bilateral'
+        );
+
+        $this->createAnswerQuestion(
+            $question3->id,
+            3,
+            'Visualização ultrassonográfica de tubo orotraqueal em traqueia'
+        );
+
+        $this->createAnswerQuestion(
+            $question3->id,
+            4,
+            'Presença de vapor em tubo orotraqueal e ausculta de região epigástrica com som rude presente'
+        );
+
+        $question4 = $this->createQuestionModule(
+            $module3->id, 
+            4, 
+            3, 
+            'Durante a ausculta pulmonar, nota-se que não tem murmúrio vesicular em hemitórax esquerdo, sendo o direito, com ausculta normal. Qual afirmativa condiz com o quadro clínico.'
+        );
+
+        $this->createAnswerQuestion(
+            $question4->id,
+            1,
+            'intubação esofágica'
+        );
+        $this->createAnswerQuestion(
+            $question4->id,
+            2,
+            'pneumotórax a direita'
+        );
+
+        $this->createAnswerQuestion(
+            $question4->id,
+            3,
+            'intubação seletiva'
+        );
+
+        $this->createAnswerQuestion(
+            $question4->id,
+            4,
+            'cuff do tubo orotraqueal furado'
+        );
+
+
+        $question5 = $this->createQuestionModule(
+            $module3->id, 
+            5, 
+            1, 
+            'Durante a ausculta pulmonar, nota-se que não tem murmúrio vesicular em hemitórax esquerdo, sendo o direito, com ausculta normal. Qual afirmativa condiz com o quadro clínico.'
+        );
+
+        $this->createAnswerQuestion(
+            $question5->id,
+            1,
+            '4; 3; 2; 1.'
+        );
+        $this->createAnswerQuestion(
+            $question5->id,
+            2,
+            '3; 1; 4; 2.'
+        );
+
+        $this->createAnswerQuestion(
+            $question5->id,
+            3,
+            '1; 2; 3; 4.'
+        );
+
+        $this->createAnswerQuestion(
+            $question5->id,
+            4,
+            '4; 3; 1; 2.'
+        );
+
+
+        $question6 = $this->createQuestionModule(
+            $module3->id, 
+            6, 
+            3, 
+            'A possibilidade de intubação difícil deve ser avaliada antes da realização do procedimento de intubação orotraqueal. Qual fator não pressupõe dificuldade no procedimento. '
+        );
+
+        $this->createAnswerQuestion(
+            $question6->id,
+            1,
+            'Obesidade'
+        );
+        $this->createAnswerQuestion(
+            $question6->id,
+            2,
+            'Idade > 55 anos'
+        );
+
+        $this->createAnswerQuestion(
+            $question6->id,
+            3,
+            'Sexo feminino'
+        );
+
+        $this->createAnswerQuestion(
+            $question6->id,
+            4,
+            'História previa de apneia do sono'
+        );
+
+        $question7 = $this->createQuestionModule(
+            $module3->id, 
+            7, 
+            3, 
+            'Via aérea definitiva em pacientes Covid são considerados com potencial fator de via aérea difícil, não apenas pelo perfil do paciente, mas também pelo uso de EPIs que dificultam o procedimento, entre eles o uso de Face Shild. É interessante ter dispositivos que auxiliam para o manejo de via aérea disponíveis no serviço. Dos itens abaixo, qual é a alternativa ERRADA em relação aos dispositivos utilizados para obtenção/auxílio de VIA AEREA DEFINITIVA. '
+        );
+
+        $this->createAnswerQuestion(
+            $question7->id,
+            1,
+            'Bougie, Sonda de aspiração rigida, videolaringoscopio, tubo orotraqueal'
+        );
+        $this->createAnswerQuestion(
+            $question7->id,
+            2,
+            'Cânula orofaríngea (Guedel), fio guia, tubo orotraqueal, material de cricotireoidostomia cirúrgica'
+        );
+
+        $this->createAnswerQuestion(
+            $question7->id,
+            3,
+            'Sonda de aspiração, máscara laríngea, sonda nasofaríngea'
+        );
+
+        $this->createAnswerQuestion(
+            $question7->id,
+            4,
+            'Laringoscópio com dispositivo de vídeo, Bougie, material de cricotireoidostomia por punção'
+        );
+
+
+        $question8 = $this->createQuestionModule(
+            $module3->id, 
+            8, 
+            3, 
+            'Sobre intubação orotraqueal em pacientes com Covid-19, é correto afirmar:'
+        );
+
+
+        $this->createAnswerQuestion(
+            $question8->id,
+            1,
+            'A videolaringoscopia para intubação orotraqueal é indicada somente nos casos de intubação difícil'
+        );
+        $this->createAnswerQuestion(
+            $question8->id,
+            2,
+            'A cricotireoidostomia cirúrgica deve ser indicada nos pacientes que possuem Mallampat III e IV'
+        );
+
+        $this->createAnswerQuestion(
+            $question8->id,
+            3,
+            'A cricotireoidostomia cirúrgica deve ser preparada em todas as intubações e ser executada apenas se houver falha no procedimento de intubação (intubação difícil)'
+        );
+
+        $this->createAnswerQuestion(
+            $question8->id,
+            4,
+            'Em caso de pacientes graves, com intubação difícil, deve ser indicada a traqueostomia de urgência'
+        );
+
+
+        $question9 = $this->createQuestionModule(
+            $module3->id, 
+            9, 
+            2, 
+            'A laringoscopia/intubação difícil é possível nos pacientes com Covid-19, tanto pelo perfil de paciente acometido, quanto pela necessidade de uso de EPIs (principalmente pelo uso de Face Shield). O conhecimento de fatores e preditores de intubação difícil aumentam o sucesso da intubação orotraqueal. Os itens listados são preditores de intubação difícil, exceto:'
+        );
+
+        $this->createAnswerQuestion(
+            $question9->id,
+            1,
+            'Classificação de Mallampati graus III e IV.'
+        );
+        $this->createAnswerQuestion(
+            $question9->id,
+            2,
+            'Medidas maiores que a distância 3-3-2 (polpas digitais) da: abertura oral – entre os incisivos; assoalho da boca, espaço entre assoalho da boca e laringe, respectivamente.'
+        );
+
+        $this->createAnswerQuestion(
+            $question9->id,
+            3,
+            'Imobilidade da coluna cervical, seja por artrose ou colar cervical'
+        );
+
+        $this->createAnswerQuestion(
+            $question9->id,
+            4,
+            'Classificação Cormack-Lehane grau IV.'
+        );
+
+
+
+        $question10 = $this->createQuestionModule(
+            $module3->id, 
+            10, 
+            4, 
+            'Para realização de intubação orotraqueal em uma unidade de atendimento com caso suspeito de Covid-19. Marque a incorreta:'
+        );
+
+        $this->createAnswerQuestion(
+            $question10->id,
+            1,
+            'Para realização de intubação orotraqueal deve ter toda equipe paramentada com uso de avental, luvas, máscara N 95, touca, Face Shield ou óculos de proteção.'
+        );
+        $this->createAnswerQuestion(
+            $question10->id,
+            2,
+            'A intubação é planejada de forma multidisciplinar (médicos, enfermeiros, técnicos de enfermagem, fisioterapeutas).'
+        );
+
+        $this->createAnswerQuestion(
+            $question10->id,
+            3,
+            'O plano de intubação orotraqueal consiste em organizar os profissionais de saúde da unidade atendimento em ter, de forma acessível e prática, os materiais e medicações para intubação, assim como deixar ventilador preparado. Além disso, deve ter nas imediações materiais que auxilie nas intercorrências como materiais de cricotireoidostomia '
+        );
+
+        $this->createAnswerQuestion(
+            $question10->id,
+            4,
+            'O cuidado com equipe administrativa, equipe de higiene e equipe de segurança, não cabe no planejamento de manejo de via aérea, a presença deles nas proximidades é de responsabilidades deles, assim como orientação de paramentação.'
+        );
     }
 }
